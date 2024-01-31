f8729e369371c7b3ebf2439ad648491d30b3918a
Leonard Kinzinger <leo.kinzinger@t-online.de>
2024-01-31 16:24:22 +0100

    added logic flow for health insurance finder, moved the handling of the typeform response to the server

--- src/modules/pages/app/Health_Insurance/health_insurance.js
+++ src/modules/pages/app/Health_Insurance/health_insurance.js
@@ -4,27 +4,31 @@ export async function render(){
 
     const insurance_status = await getModuleStatus('health_insurance');
 
+    console.log(insurance_status)
+
     const public_health_insurance = document.querySelector('[w-el="public_health_insurance"]');
     const private_health_insurance = document.querySelector('[w-el="private_health_insurance"]');
     const expat_health_insurance = document.querySelector('[w-el="expat_health_insurance"]');
     const travel_health_insurance = document.querySelector('[w-el="travel_health_insurance"]');
 
+    const title = document.querySelector('.finder-result_title');
+
     if (insurance_status.has_taken_quiz) {
-        // Do nothing for now
+        title.classList.remove('hide');
     } else {
         // Do nothing for now
     }
 
-    if (insurance_status.needs_public_health_insurance) {
+    if (insurance_status.qualifies_public_health_insurance) {
         public_health_insurance.classList.remove('hide');
     }
-    if (insurance_status.needs_private_health_insurance) {
+    if (insurance_status.qualifies_private_health_insurance) {
         private_health_insurance.classList.remove('hide');
     } 
-    if (insurance_status.needs_expat_insurance) {
+    if (insurance_status.qualifies_expat_insurance) {
         expat_health_insurance.classList.remove('hide');
     } 
-    if (insurance_status.needs_travel_insurance) {
+    if (insurance_status.qualifies_travel_insurance) {
         travel_health_insurance.classList.remove('hide');
     } 
     
--- src/modules/pages/app/Health_Insurance/health_insurance_finder.js
+++ src/modules/pages/app/Health_Insurance/health_insurance_finder.js
@@ -1,83 +1,17 @@
 import { createWidget } from '@typeform/embed';
 import '@typeform/embed/build/css/widget.css';
-import { get_tf_result } from '../../../utils';
-import { getCookie } from '../../../auth';
+import { attachDatePicker, get_tf_result } from '../../../utils';
+import { getAccountSettings, getCookie } from '../../../auth';
+import { autoSaveFunction, debounce, fillFieldsFromDatabase } from '../../../form_handling';
 
-async function submit_health_insurance(response) {
+
+async function submit_health_insurance(form_id,response_id) {
 
     const token = getCookie('wized_token');
     const formData = new FormData ();
 
-    formData.append('has_taken_quiz',1);
-
-    response.variables.forEach(variable => {
-        if (variable.key === "expat_insurance") {
-            formData.append('qualifies_expat_insurance',variable.number);
-        } else if (variable.key === "private_insurance") {
-            formData.append('qualifies_public_health_insurance',variable.number);
-        } else if (variable.key === "public_insurance") {
-            formData.append('qualifies_private_health_insurance',variable.number);
-        } 
-    })
-
-    response.answers.forEach(answer => {
-        console.log(answer.field.ref, answer.text);
-        switch (answer.field.ref) {
-            case 'answer_citizenship': 
-                formData.append('citizenship',answer.text);
-                break;
-            case 'answer_citizenship_2': 
-                formData.append('citizenship_2',answer.text);
-                break;
-            case 'answer_birth_date': 
-                formData.append('birth_date',answer.date);
-                break;
-            case 'answer_country_residence': 
-                formData.append('residence_country',answer.text);
-                break;
-            case 'answer_relocation_date': 
-                formData.append('relocation_date',answer.date);
-                break;
-            case 'answer_relocation_date_2': 
-                formData.append('relocation_date',answer.date);
-                break;
-            case 'answer_health_insurance_status': 
-                if (answer.choice.label ==='No') {
-                    formData.append('current_health_insurance_status','None');
-                } else if (answer.choice.label ==='Only on Expat or Travel Insurance') {
-                    //handle expat and travel insurance                
-                } 
-                break;
-            case 'answer_current_health_insurance': 
-                if (answer.choice.label ==='Public Insurance (EU/EAA, not Germany)') {
-                    formData.append('current_health_insurance_status',answer.choice.label);
-                } else if (answer.choice.label ==='Public Insurance from Germany') {
-                    formData.append('current_health_insurance_status','Public Insurance Germany');
-                } else if (answer.choice.label ==='Private Insurance from (EU/EAA, not Germany)') {
-                    formData.append('current_health_insurance_status',answer.choice.label);              
-                } else if (answer.choice.label ==='Private Insurance from Germany') {
-                    formData.append('current_health_insurance_status','Private Insurance Germany');              
-                }
-                break;
-            case 'answer_occupation': 
-                formData.append('occupation_type',answer.choice.label);
-                break;
-            case 'answer_student_status': 
-                formData.append('student_status',answer.choice.label);
-                break;
-            case 'answer_annual_income': 
-                formData.append('income',answer.number);
-                break;
-            default:
-                break;
-        }
-    });
-
-    for (const pair of formData.entries()) {
-        console.log('Form Data: ');
-        console.log(`${pair[0]}, ${pair[1]}`);
-      }
-
+    formData.append('form_id',form_id);
+    formData.append('response_id',response_id);
 
     try {
         // Make a GET request to the authentication endpoint with the token in the headers.
@@ -108,24 +42,97 @@ async function submit_health_insurance(response) {
     }
 }
 
-export function render () {
+async function setupTypeform(identifier, typeform_id,hidden) {
+
+    const query_selector = `[w-el="`+identifier+`"]`
 
     const options = {
-        container: document.querySelector('[w-el="typeform_quiz"]'), 
+        container: document.querySelector(query_selector), 
+        hidden: hidden,
         hideHeaders: true,
         hideFooter: true,
         opacity: 0,
         height : 600,
         onSubmit: async ({ formId, responseId }) =>  {
             console.log(`Form ${formId} submitted, response id: ${responseId}`)
-            const response = await get_tf_result(formId,responseId);
-            console.log(response);
-            const updated_user = await submit_health_insurance(response);
+            //const response = await get_tf_result(formId,responseId);
+            //console.log(response);
+            const updated_user = await submit_health_insurance(formId,responseId);
             console.log(updated_user);
           },
     }
 
-    createWidget('siLiU31K', options)
+    createWidget(typeform_id, options)
 
 }
+export function render () {
 
+    try {
+        const slide1_externalData = document.querySelector('[w-el="slide1_externalData"]');
+        const slide2_reviewData = document.querySelector('[w-el="slide2_reviewData"]');
+        const typeform_quiz = document.querySelector('[w-el="typeform_quiz"]');
+
+
+        document.querySelector('[w-el="prefill_yes"]').addEventListener('click', async function() {
+            // Code to handle "Yes" response
+            console.log("User consented to use external data.");
+            slide1_externalData.classList.add('hide');
+            slide2_reviewData.classList.remove('hide');
+
+            attachDatePicker();
+            const response = await getAccountSettings(getCookie('wized_token'));
+
+            if (response.success) {
+                fillFieldsFromDatabase(response.user, 'account_');
+                const formWrapper = document.querySelector('[w-el="formWrapper"]');
+
+                if (formWrapper) {
+                    formWrapper.addEventListener('change', debounce((event) => autoSaveFunction(event, `${BASE_URL}/account_settings`), 500));
+                } else {
+                    logging.warning({
+                        message: 'render: Form wrapper not found',
+                        eventName: 'render_no_formWrapper',
+                    });
+                }
+            } else {
+                logging.error({
+                    message: 'render: Failed to fetch account settings: ' + response.message,
+                    eventName: 'render_fetch_error',
+                    extra: {}
+                });
+                return;
+            }
+        });
+        document.querySelector('[w-el="proceedToQuiz"]').addEventListener('click', async function() {
+            // Code to handle "No" response
+            const response = await getAccountSettings(getCookie('wized_token'));
+            console.log(response)
+            console.log("User proceeds to quiz.");
+            slide2_reviewData.classList.add('hide');
+            typeform_quiz.classList.remove('hide');
+            const hidden_data = {
+                'citizenship':response.user.citizenship,
+                'citizenship_2':response.user.citizenship_2,
+                'residence_country':response.user.residence_country,
+                'relocation_date':response.user.relocation_date,
+                'birth_date':response.user.birth_date,
+                'birth_date_is_before_1969_01_01': response.user.birth_date ? new Date(response.user.birth_date) < new Date('1969-01-01') : null
+            }
+            await setupTypeform("typeform_quiz","siLiU31K",hidden_data);
+        });
+
+
+        
+        document.querySelector('[w-el="prefill_no"]').addEventListener('click', async function() {
+            // Code to handle "No" response
+            console.log("User did not consent to use external data.");
+            slide1_externalData.classList.add('hide');
+            typeform_quiz.classList.remove('hide');
+            await setupTypeform("typeform_quiz","siLiU31K",{});
+        });
+
+
+    } catch (error) {
+        
+    }
+}
