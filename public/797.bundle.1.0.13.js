cbff91b561d72ab94fa374c84a96ab8598e1596b
Leonard Kinzinger <leo.kinzinger@t-online.de>
2024-02-28 16:28:45 +0100

    add new bundles for path 13

---
+++ public/797.bundle.1.0.13.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkworldify=self.webpackChunkworldify||[]).push([[797],{452:(e,r,n)=>{n.r(r),n.d(r,{render:()=>o});var t=n(444),c=n(797);async function o(){try{const e=document.querySelector(".section_upload"),r=20,n=e.querySelector(".document_list-wrapper"),o=e.querySelector(".button");(0,t.lC)(n,r),(0,t.ab)(n,r),o&&o.addEventListener("click",(function(){(0,c.bA)(element.submoduleId)}))}catch(e){logging.error({message:"Error in vault render function: "+e.message,eventName:"render_exception",extra:{}})}}}}]);
+//# sourceMappingURL=797.bundle.1.0.13.js.map
